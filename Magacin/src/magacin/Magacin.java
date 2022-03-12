c2c3bc4a3fa53625d6cd9918a6b75f442dbee7c5
vasivuk <vasivuk@yahoo.com>
2022-03-12 12:41:33 +0100

    Klasa Magacin implementira MagacinInterfejs, metode prazne

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -3,7 +3,27 @@ package magacin;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Magacin {
+import magacin.interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs{
 	
 	List<Artikal> artikli = new LinkedList<Artikal>();
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(long sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
